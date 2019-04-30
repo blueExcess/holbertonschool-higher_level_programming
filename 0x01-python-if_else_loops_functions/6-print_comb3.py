1534393b6eac14c90636384ad8b95213746c88b2
Nate Petersen <nate.petersen@me.com>
2019-04-30 20:17:33 +0000

    finished 0, 1, 5, 6 (idk why, deal with it)

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(1, 10):
+	for x in range(i, 10):
+		if (i-1) == 8 and x == 9:
+			print("{:d}{:d}".format((i-1), x))
+		else:
+			print("{:d}{:d}, ".format((i-1), x), end="")
