16a3306a0f0618e5dd3c96f3bd967304e9c1bca2
Nate Petersen <nate.petersen@me.com>
2019-04-30 21:02:34 +0000

    weird spacing bug

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,9 +2,9 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-   x = number % -10
+    x = number % -10
 else:
-   x = number % 10
+    x = number % 10
 print("Last digit of {:d} is {:d}".format(number, x), end=' ')
 if x > 5:
     print("and is greater than 5")
