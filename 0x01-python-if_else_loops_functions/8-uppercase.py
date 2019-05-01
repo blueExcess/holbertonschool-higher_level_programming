026cee99e62698981eb85fa9c45fb28394d271db
Nate Petersen <nate.petersen@me.com>
2019-05-01 01:22:38 +0000

    add __* to gitignore, fixed line errors on 7 and 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 # mimic upper()
+
+
 def uppercase(str):
     for i in str:
-        if ord(i) < 97 and ord(i) > 122:
-            value = ord(i)
-        else:
+        if ord(i) > 96 and ord(i) < 123:
             value = ord(i) - 32
+        else:
+            value = ord(i)
         print("{:c}".format(value), end="")
     else:
         print()
