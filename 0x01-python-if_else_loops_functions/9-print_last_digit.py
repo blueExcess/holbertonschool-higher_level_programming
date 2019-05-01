90fd6b4c463f69e1aea7f5e93ffc80ffbc3bcc2a
Nate Petersen <nate.petersen@me.com>
2019-05-01 01:31:51 +0000

    fix silly mistake (part of function outside function)

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 # function to print and return last digit of a number
 
+
 def print_last_digit(number):
     if number < 0:
         d = number % -10
+        d = -d
     else:
         d = number % 10
-print("{:d}".format(d), end="")
-return (d)
+    print("{:d}".format(d), end="")
+    return (d)
