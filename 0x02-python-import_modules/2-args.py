08016896128d216e3c9a8e33b605a8e8db7f469a
Nate Petersen <nate.petersen@me.com>
2019-05-02 22:06:39 +0000

    complete 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+
+from sys import argv
+if __name__ == '__main__':
+    count = 0
+    if len(argv) == 1:
+        print("0 arguments.")
+        exit()
+    narg = len(argv) - 1
+    print('{} arguments:'.format(narg))
+    for x in argv:
+        count += 1
+        if count == 1:
+            continue
+        print('{}: {}'.format(count - 1, x))
