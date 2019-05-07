870a1635c0c3017d1d5e1caf4456ae57494d9fe5
Nate Petersen <nate.petersen@me.com>
2019-05-07 21:06:32 +0000

    complete 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    ret = []
+    for i in my_list:
+        if i % 2 == 0:
+            ret.append(True)
+        else:
+            ret.append(False)
+    return ret
