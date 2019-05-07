03b7cbaef2bb778b72157dc168d9e67f08969f2f
Nate Petersen <nate.petersen@me.com>
2019-05-07 18:34:17 +0000

    complete 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    newString = list(my_string)
+    for i in range(len(newString)):
+        if newString[i] == 'c' or newString[i] == 'C':
+            newString[i] = ""
+    my_string = "".join(newString)
+    return my_string
