b002882c036a640c95f7a4ad77d9149fa12bde98
Nate Petersen <nate.petersen@me.com>
2019-05-07 19:21:28 +0000

    complete 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,2 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i, x in enumerate(row):
+            print("{:d}".format(x), end="" if i == len(row) - 1 else " ")
+        print()
