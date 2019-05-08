fa6c3fb47f90bff81e7e005960651243c4262ac2
Nate Petersen <nate.petersen@me.com>
2019-05-08 02:39:56 +0000

    first attempt at 13

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,35 @@
+/* 13. Test if a LL is also a palindrome */
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * is_palindrome - test if a LL is also a palindrome
+ * @head: begining of list
+ *
+ * Return: 1 for yes, 0 for no or fail
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *seek = *head;
+	int i = 0, j = 0, test = 1;
+	int arr[20];
+
+
+	if (!head)
+		return (0);
+	if (!*head)
+		return (1);
+	for (; seek; seek = seek->next, i)
+		arr[i] = seek->n;
+	for (; i > j; i--, j++)
+	{
+		if (arr[i] == arr[j])
+			test = 1;
+		else
+			test = 0;
+	}
+	if (test != 1)
+		return (0);
+	return (1);
+}
