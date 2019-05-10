a83f2b894d02e827f1fab2834622d08de5362972
Nate Petersen <nate.petersen@me.com>
2019-05-10 03:37:08 +0000

    first draft of 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
-def weight_average(my_list=[]):
+def roman_to_int(rs):
+    if rs is None or isinstance(rs, str) == False:
+        return 0
+    val, total = 0, 0
+    nums = []
+    legend = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+              'C': 100, 'D': 500, 'M': 1000}
+    for i, num in enumerate(rs):
+        nums.append(legend.get(num))
+        total += nums[i]
+        if i > 0:
+            if nums[i] > nums[i - 1]:
+                total -= (1 + nums[i - 1])
+    return total
