5be9b89b0faef6b68a2b456661d7bb9af6ac03bb
Nate Petersen <nate.petersen@me.com>
2019-05-10 02:33:40 +0000

    forgot to cover empty list case

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
     isum, weight = 0, 0
-    if my_list is None:
+    if my_list is None or len(my_list) < 1:
         return 0
     for x in my_list:
         isum += (x[0] * x[1])
