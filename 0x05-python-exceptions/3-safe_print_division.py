ef8ec36aa089930b59297b56b92a31e5e8b83e83
Nate Petersen <nate.petersen@me.com>
2019-05-20 18:26:23 +0000

    add return statement

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    result = 0
     try:
-        result = a / b
+        c = a / b
     except ZeroDivisionError:
-        result = None
+        c = None
     finally:
-        print("Inside result: {}".format(result))
+        print("Inside result: {}".format(c))
+    return c
