9a0f30149cd267c83e9a5fa66c2117fbf426cd03
Nate Petersen <nate.petersen@me.com>
2019-05-20 16:56:37 +0000

    fix not printing newline

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    count = 0
     try:
         for i in range(0, x):
             print(my_list[i], end="")
-        return i + 1
+            count += 1
+        print()
+        return count
     except IndexError:
-        return i
+        print()
+        return count
