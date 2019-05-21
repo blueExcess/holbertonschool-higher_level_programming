5b4e5852c80a99e7af4a55e3e3aa497bd228eb1b
Nate Petersen <nate.petersen@me.com>
2019-05-21 18:21:27 +0000

    update formatting on documentation for 1 and complete 2

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,7 +3,7 @@ class Square:
     """Class square with one attribute.
 
     Args:
-    size: size of square
+        size: size of square
     """
     def __init__(self, size):
         self.__size = size
