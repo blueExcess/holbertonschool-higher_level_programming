660621ecc58425e44fb812daae3aa744786b2131
Nate Petersen <nate.petersen@me.com>
2019-05-21 21:18:14 +0000

    removed error checking from __init__ on 5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -12,10 +12,10 @@ class Square:
     def __init__(self, size=0):
         """Inits class with given size."""
         self.__size = size
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
+        # if not isinstance(size, int):
+        #     raise TypeError("size must be an integer")
+        # elif size < 0:
+        #     raise ValueError("size must be >= 0")
 
     def area(self):
         """Calculates area of square.
