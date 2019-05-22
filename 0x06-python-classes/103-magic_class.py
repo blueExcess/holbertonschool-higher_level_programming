76486bcecda0c758281e4ace2c249768c0def6cc
Nate Petersen <nate.petersen@me.com>
2019-05-22 04:14:51 +0000

    complete 103

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+""" some comment """
+import math
+
+
+class MagicClass:
+    """Comment for class."""
+    def __init__(self, radius=0):
+        """comment for init"""
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError('radius must be a number')
+        self.__radius = radius
+
+    def area(self):
+        """Def for Area stuff."""
+        return self.__radius ** 2 * math.pi
+
+    def circumference(self):
+        """Def for this stuff."""
+        return (2 * math.pi) * self.__radius
