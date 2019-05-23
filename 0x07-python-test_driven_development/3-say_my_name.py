fd74f64b21406ef6e4852123861f12c3b36f7f2a
Nate Petersen <nate.petersen@me.com>
2019-05-23 21:26:12 +0000

    completed task three and test document

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Function that prints a person's name."""
+
+
+def say_my_name(first_name, last_name=""):
+    if not isinstance(first_name, str):
+        raise TypeError('first_name must be a string')
+    elif not isinstance(last_name, str):
+        raise TypeError('last_name must be a string')
+    else:
+        print('My name is', first_name, last_name)
