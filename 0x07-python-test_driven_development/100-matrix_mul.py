43b794f0c8d11f61a4ca8a19918c3d15719a4251
Nate Petersen <nate.petersen@me.com>
2019-05-25 04:15:42 +0000

    draft of 101 and frame of 100

---
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""Function to multiply two matrices."""
+
+
+def matrix_mul(m_a, m_b):
+    """Multiply two matrices.
+
+    Args:
+        m_a: First matrix - must be a list of lists of ints or floats.
+        m_b: Second matrix - must be a list of lists of ints or floats.
+
+    Raises:
+        TypeError: raised if input is incorrect.
+        ValueError: raised if values of input is incorrect.
+    """
+
+    recA = []
+    recB = []
+    if type(m_a) is not list:
+        raise TypeError('m_a must be a list')
+    if type(m_b) is not list:
+        raise TypeError('m_b must be a list')
+    for x in m_a:
+        recA.append(len(x))
+        if type(x) is not list:
+            raise TypeError('m_a must be a list of lists')
+        elif len(x) == 0:
+            raise ValueError("m_a can't be empty")
+    for x in m_b:
+        recB.append(len(x))
+        if type(x) is not list:
+            raise TypeError('m_b must be a list of lists')
+        elif len(x) == 0:
+            raise ValueError("m_b can't be empty")
+    if len(m_a) == 0:
+        raise ValueError("m_a can't be empty")
+    if len(m_b) == 0:
+        raise ValueError("m_b can't be empty")
+    for row in m_a:
+        for value in row:
+            if type(value) is not int or type(value) is not float:
+                raise TypeError('m_a should contain only integers or floats')
+    for row in m_b:
+        for value in row:
+            if type(value) is not int or type(value) is not float:
+                raise TypeError('m_b should contain only integers or floats')
+    if len(set(recA)) != 1:
+        raise TypeError("each row of m_a must should be of the same size")
+    if len(set(recB)) != 1:
+        raise TypeError("each row of m_b must should be of the same size")
+
+    for x in m_a:
+        
---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+import numpy as np
+"""Function to multiply two matrices."""
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """Multiply two matrices.
+
+    Args:
+        m_a: First matrix - must be a list of lists of ints or floats.
+        m_b: Second matrix - must be a list of lists of ints or floats.
+
+    Raises:
+        TypeError: raised if input is incorrect.
+        ValueError: raised if values of input is incorrect.
+    """
+
+    recA = []
+    recB = []
+    if type(m_a) is not list:
+        raise TypeError
+    if type(m_b) is not list:
+        raise TypeError
+    for x in m_a:
+        recA.append(len(x))
+        if type(x) is not list:
+            raise TypeError
+        elif len(x) == 0:
+            raise ValueError
+    for x in m_b:
+        recB.append(len(x))
+        if type(x) is not list:
+            raise TypeError
+        elif len(x) == 0:
+            raise ValueError
+    if len(m_a) == 0:
+        raise ValueError
+    if len(m_b) == 0:
+        raise ValueError
+    for row in m_a:
+        for value in row:
+            if type(value) is not int or type(value) is not float:
+                raise TypeError
+    for row in m_b:
+        for value in row:
+            if type(value) is not int or type(value) is not float:
+                raise TypeError
+    if len(set(recA)) != 1:
+        raise TypeError
+    if len(set(recB)) != 1:
+        raise TypeError
+    a = np.array(m_a)
+    b = np.array(m_b)
+    print(np.dot(a, b))
