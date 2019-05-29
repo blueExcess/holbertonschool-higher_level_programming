16d7be7d6b62e1178d4953efbdbda80f64be0f60
Nate Petersen <nate.petersen@me.com>
2019-05-29 03:36:48 +0000

    exceptions for 101

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""module for function to find solution to n queens problem."""
+from sys import argv
+
+
+def queens():
+    """Find solution to N Queens problem."""
+    if len(argv) != 2:
+        print("Usage: nqueens N")
+        exit(1)
+    if not isinstance(argv[1], int):
+        print('N must be a number')
+        exit(1)
+    elif argv[1] < 4:
+        print('N must be at least 4')
+        exit(1)
