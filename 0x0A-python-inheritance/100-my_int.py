4a8034b1f7c1e0d7036716d6448ffcc3a495b39f
Nate Petersen <nate.petersen@me.com>
2019-06-06 03:01:37 +0000

    complete task 12 (100)

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""Contains class to do annoying things."""
+
+
+class MyInt(int):
+    """Will switch around the == and != operators. Tee hee hee."""
+
+    def __eq__(self, value):
+        """Return wrong operator."""
+        return super().__ne__(self)
+
+    def __ne__(self, value):
+        """Returns the equals operator."""
+        return super().__eq__(self)
