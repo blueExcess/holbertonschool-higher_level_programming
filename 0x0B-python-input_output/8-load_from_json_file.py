64368fd24738222fdcef1f7f4a26d9b7604a3970
Nate Petersen <nate.petersen@me.com>
2019-06-06 21:01:39 +0000

    complete tasks 8 and 9

--- 0x0B-python-input_output/8-load_from_json_file.py
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
+"""8. function to create object from json file."""
+import json
+
+
 def load_from_json_file(filename):
+    """return object from json file."""
+    with open(filename, 'r') as f:
+        return json.load(f)
--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,2 +1,22 @@
 #!/usr/bin/python3
-def class_to_json(obj):
+"""add all arguments to a python list and save to a file."""
+import json
+from sys import argv
+loadff = __import__('8-load_from_json_file').load_from_json_file
+dumptf = __import__('7-save_to_json_file').save_to_json_file
+
+
+name = 'add_item.json'
+try:
+    f = open(name)
+except:
+    li = []
+    for x in argv[1:]:
+        li.append(x)
+    dumptf(li, name)
+else:
+    f.close()
+    jli = loadff(name)
+    for x in argv[1:]:
+        jli.append(x)
+    dumptf(jli, name)
