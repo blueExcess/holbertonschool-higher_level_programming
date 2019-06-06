dac370b1111c1a63040bfeec57850138e0d9fe04
Nate Petersen <nate.petersen@me.com>
2019-06-06 01:47:46 +0000

    complete task 6

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""Class that does something."""
+
+
+class BaseGeometry():
+    """A class that will do something, presumably."""
+    def area(self):
+        """Not implemented."""
+        raise Exception('area() is not implemented')
