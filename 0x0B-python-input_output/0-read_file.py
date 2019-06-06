b535021e09458647e0c472cd708017af3224549a
Nate Petersen <nate.petersen@me.com>
2019-06-06 23:54:15 +0000

    remove \n on 0 and complete task 13

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """read a file and print to stdout."""
     with open(filename) as f:
-        print(f.read())
+        print(f.read(), end='')
--- 0x0B-python-input_output/13-student.py
+++ 0x0B-python-input_output/13-student.py
@@ -1,1 +1,37 @@
 #!/usr/bin/python3
+"""11. Class for Student that does stuff."""
+
+
+class Student():
+    """does json type stuff.
+
+    Args:
+        first_name (str): first name of student.
+        last_name (str): last name of student.
+        age (int): age of student.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns __dict__ of object."""
+        x = self.__dict__
+        if attrs is None:
+            return self.__dict__
+        if not type(attrs) is list:
+            return self.__dict__
+        for i in attrs:
+            if not type(i) is str:
+                return self.__dict__
+        z = {}
+        for k, v in x.items():
+            if k in attrs:
+                z.update({k: v})
+        return z
+
+    def reload_from_json(self, json):
+        """take dict object and overwrite attributes with new ones."""
+        self.__dict__ = json
