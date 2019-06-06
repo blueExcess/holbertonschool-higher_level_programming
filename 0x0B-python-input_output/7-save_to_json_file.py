4b0b9bd530b7c6588c5ac5618e2e79db1309d9c7
Nate Petersen <nate.petersen@me.com>
2019-06-06 20:06:40 +0000

    complete task 7

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
+"""7. write json to txt file."""
+import json
+
+
 def save_to_json_file(my_obj, filename):
+    """converts to json and saves to a file."""
+    with open(filename, 'wt') as f:
+        json.dump(my_obj, f)
