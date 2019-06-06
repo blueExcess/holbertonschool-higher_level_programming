f167fccf0d2e9702a1a70f41c89fb513fb38736f
Nate Petersen <nate.petersen@me.com>
2019-06-06 03:28:22 +0000

    attempt task 13 (101). Not quite there yet...

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""Adds a function to try and add stuff to stuff."""
+
+
+def add_attribute(obj, name, value):
+    """Attempts to add a value to an object.
+
+    Args:
+        obj: object to try.
+        name (str): name of attribute to add.
+        value: value to add.
+
+    Raises:
+        TypeError: if obj can't accept new attributes.
+    """
+
+    if hasattr(obj, __dict__):
+        setattr(obj, name, value)
+    else:
+        raise TypeError("can't add new attribute")
