18681a4c597f94c3fa7398118cd2cb20fe1d5ca6
Nate Petersen <nate.petersen@me.com>
2019-06-06 01:35:03 +0000

    complete task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
+"""Module definininininin lalallaa."""
+
+
 def inherits_from(obj, a_class):
+    """Return what the funciton inherits from."""
+    if isinstance(obj, a_class) and type(obj) is not a_class:
+        return True
+    else:
+        return False
