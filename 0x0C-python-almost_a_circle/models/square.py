135d6919a7fce60b4082cde4909c6628c0de5274
Nate Petersen <nate.petersen@me.com>
2019-06-12 16:08:46 +0000

    complete task 11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -11,9 +11,19 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """Getter for size."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setter for size - based off width and height."""
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """Return the proper string."""
         return "[{}] ({}) {}/{} - {}".format(type(self).__name__, self.id,
                                              self.x, self.y, self.width)
 
-        
