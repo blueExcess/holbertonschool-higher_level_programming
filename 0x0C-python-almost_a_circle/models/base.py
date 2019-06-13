0370e81c3c9d37bd3d64bad8d4f56132c58c90e0
Nate Petersen <nate.petersen@me.com>
2019-06-13 03:33:12 +0000

    tweaks to turtle method in base and finished last method in base.

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -70,16 +70,26 @@ class Base():
     def load_from_file(cls):
         """Returns a list of instances."""
 
-        if os.path.isfile("{}.json".format(cls.__name__)):
-            pass
+        if not os.path.isfile("{}.json".format(cls.__name__)):
+            return []
+        with open("{}.json".format(cls.__name__), 'rt') as f:
+            out = cls.from_json_string(f.read())
+            li = []
+            for thing in out:
+                li.append(cls.create(**thing))
+            return li
 
+    @staticmethod
     def draw(list_rectangles, list_squares):
         """Will open a window and draw a lovely turtle."""
-        colors = []
+        colors = ['blue', 'red', 'green', 'purple', 'orange', 'yellow',
+                  'black', 'red', 'purple', 'pink']
         win = turtle.Screen()
         win.title("Dumb Squares")
         t = turtle.Turtle()
+        i = 0
         for cls in list_rectangle + list_squares:
+            t.color(colors[i])
             t.penup()
             t.goto(cls.x, cls.y)
             t.pendown()
@@ -91,3 +101,4 @@ class Base():
             t.forward(cls.width)
             t.right(90)
             t.forward(cls.height)
+            i += 1
