2cd9d9539f24d18dc4852c9b3095629a74e3feaf
Nate Petersen <nate.petersen@me.com>
2019-07-15 20:32:41 +0000

    change EXIST to EXISTS

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,6 +1,6 @@
 -- 4. create a new table in current db
 -- will be called first_table
-CREATE TABLE IF NOT EXIST first_table (
+CREATE TABLE IF NOT EXISTS first_table (
        id INT,
        name VARCHAR(256)
        );
