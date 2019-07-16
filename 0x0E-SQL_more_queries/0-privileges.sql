48259bb4fb36db197904823c714ea2ac6c34aa9b
Nate Petersen <nate.petersen@me.com>
2019-07-16 21:31:18 +0000

    add FOR keyword because apparently all the resources lie - thanks assholes.

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- 0. Show permissions granted to users
 -- GRANTS
-SHOW GRANTS user_0d_1@localhost;
-SHOW GRANTS user_0d_2@localhost;
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
