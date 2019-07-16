5cac0ec3aca107710bcf53e30aa90bc235b70745
Nate Petersen <nate.petersen@me.com>
2019-07-16 22:15:00 +0000

    complete task 2 - create table and grant permissions to new user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,2 +1,7 @@
 -- 2. create a database and a user
 -- only select priviledge and no fail.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2@localhost'
+IDENTIFIED BY 'user_0d_2_pwd';
+
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2@localhost';
