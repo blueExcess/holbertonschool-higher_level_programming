54df51237cd07a3ea6394f59e9505d453b282365
Nate Petersen <nate.petersen@me.com>
2019-08-09 21:52:21 +0000

    complete task 2 - pass in value to search for in table

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+# list all matches to given argument (fourth arg)
+
+
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    username, password, database, state = argv[1:]
+
+    db = MySQLdb.connect(host="localhost", user=username,
+                         passwd=password, db=database)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE '{}'".format(state))
+
+    querry = cur.fetchall()
+    for row in querry:
+        print(row)
+
+    cur.close()
+    db.close()
