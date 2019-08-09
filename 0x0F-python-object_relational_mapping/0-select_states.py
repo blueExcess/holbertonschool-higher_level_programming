69f2026a14e1755e9cc2c77b9f46081534faf520
Nate Petersen <nate.petersen@me.com>
2019-08-09 21:30:34 +0000

    complete task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+# script to connect to mysql database in python
+
+
+from sys import argv
+import MySQLdb
+
+
+if __name__ == "__main__":
+    username, password, database = argv[1:]
+
+    db = MySQLdb.connect(host="localhost", user=username,
+                         passwd=password, db=database)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+
+    querry = cur.fetchall()
+    for row in querry:
+        print(row)
+
+    cur.close()
+    db.close()
