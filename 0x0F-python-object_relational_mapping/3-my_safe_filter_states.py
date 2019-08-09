2618dc51792b03716f6c8c07f182f093491982fa
Nate Petersen <nate.petersen@me.com>
2019-08-09 22:45:14 +0000

    complete tasks 3 and 4 - safe mode and city search

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+# Make #2 safe from sql injection
+
+
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    username, password, database, state = argv[1:]
+
+    db = MySQLdb.connect(host="localhost", user=username,
+                         passwd=password, db=database)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE %s", (state,))
+
+    querry = cur.fetchall()
+    for row in querry:
+        print(row)
+
+    cur.close()
+    db.close()
--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+# Make #2 safe from sql injection
+
+
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    username, password, database = argv[1:]
+
+    db = MySQLdb.connect(host="localhost", user=username,
+                         passwd=password, db=database)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM cities")
+
+    querry = cur.fetchall()
+    for row in querry:
+        print(row)
+
+    cur.close()
+    db.close()
