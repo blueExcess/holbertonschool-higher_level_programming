7383691e4c043b3ac66e644228dd9dd95833af81
Nate Petersen <nate.petersen@me.com>
2019-08-10 03:19:29 +0000

    change querry from select all to merge lists in task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -11,7 +11,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=username,
                          passwd=password, db=database)
     cur = db.cursor()
-    cur.execute("SELECT * FROM cities")
+    cur.execute("SELECT cities.id, cities.name, states.name \
+    FROM hbtn_0e_4_usa.cities \
+    INNER JOIN hbtn_0e_0_usa.states ON states.id = cities.state_id")
 
     querry = cur.fetchall()
     for row in querry:
