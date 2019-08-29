3e3452959631b29921151206e40cdae9fecd6598
Nate Petersen <nate.petersen@me.com>
2019-08-29 20:31:56 +0000

    fix pep8 - split into multiple print statements to get formatting correct

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,5 +7,6 @@ if __name__ == '__main__':
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
 
-        print("Body response:\n\t- type: %s\n\t- content: %s\n
-        \t- utf8 content: %s".format(type(html), html, html.decode('utf-8')))
+        print("Body response:\n\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
