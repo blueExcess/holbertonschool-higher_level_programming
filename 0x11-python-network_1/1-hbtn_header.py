b5c6cdfd4067f53bedfb0d50c1c4701dfbd88726
Nate Petersen <nate.petersen@me.com>
2019-08-30 16:53:04 +0000

    complete task 1 - get something from header

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,5 +2,8 @@
 """ 1. send request """
 
 import urllib.request
+import sys
 
-
+if __name__ == '__main__':
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        print(response.headers.get("X-Request-Id"))
