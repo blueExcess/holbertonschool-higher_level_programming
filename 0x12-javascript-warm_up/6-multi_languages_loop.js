5f3a972e55190a55e894f87e473f06544c0dda14
Nate Petersen <nate.petersen@me.com>
2019-09-17 20:02:43 +0000

    complete task 6 - make a loop and print elements of an array.

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+/* 6. print using array of strings in a loop */
+const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
