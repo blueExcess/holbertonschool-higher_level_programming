34092135b3558f40b9f64cda7987e7d41b320fa9
Nate Petersen <nate.petersen@me.com>
2019-09-17 21:48:54 +0000

    complete task 11 - convert args to ints in array and return second-largest number

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+/* 11. find second-largest argument given. */
+const args = process.argv.slice(2);
+const len = args.length;
+if (len <= 1) {
+  console.log(0);
+  return;
+}
+
+// let bigger = args[0];
+// let big = args[1];
+let bigger = parseInt(args[0], 10);
+let big = parseInt(args[1], 10);
+if (bigger < big) {
+  bigger = args[1];
+  big = args[0];
+}
+
+for (let i = 2; i < len; i++) {
+  let x = parseInt(args[i], 10);
+  if (bigger < x) {
+    big = bigger;
+    bigger = x;
+  }
+}
+console.log(big);
