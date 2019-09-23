42d4aad04298d4310543f770412e66960f6e6b65
Nate Petersen <nate.petersen@me.com>
2019-09-23 15:19:52 +0000

    complete task 3 - add method 'print()' that prints a representation of the rectangle using 'X'.

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+/* 3. create method 'print()' that prints representation of object. */
+
+class Rectangle {
+  constructor(w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print() {
+    const str = 'X';
+    const line = str.repeat(this.width);
+    for (let i = 0; i < this.height; i++) {
+      console.log(line);
+    }
+  }
+}
+
+module.exports = Rectangle
