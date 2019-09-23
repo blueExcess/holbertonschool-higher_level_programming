66a3144a0f7af05971d46bbbc7b08a9fe2654667
Nate Petersen <nate.petersen@me.com>
2019-09-23 18:22:35 +0000

    fix semistandard issues on 7

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -2,11 +2,11 @@
 /* write a function that returns number of occurances in a list. */
 
 exports.nbOccurences = function (list, searchElement) {
-  count = 0;
+  let count = 0;
   for (let i = 0; i < list.length; i++) {
     if (list[i] === searchElement) {
       count += 1;
     }
   }
   return count;
-}
+};
