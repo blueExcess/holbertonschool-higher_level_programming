8f8e0557b873ce331179b68d0a2fef74cbd3ad9c
Nate Petersen <nate.petersen@me.com>
2019-09-23 19:06:27 +0000

    move 'count' into else statement so count starts at 0.

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -4,7 +4,8 @@
 exports.logMe = function (item) {
   if (typeof exports.logMe.count === 'undefined') {
     exports.logMe.count = 0;
+  } else {
+    exports.logMe.count += 1;
   }
-  exports.logMe.count += 1;
   console.log(exports.logMe.count + ': ' + item);
 };
