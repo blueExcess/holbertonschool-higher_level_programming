78cd3d63aae0d20a6d6a13b589c0110906714c23
Nate Petersen <nate.petersen@me.com>
2019-09-23 17:58:52 +0000

    complete task 6 - inherit from Square (for a new class named Square? uhh...) and change print method.

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+/* 5. Write class Square that inherits from Rectangle. */
+
+class Square extends require('./5-square') {
+  charPrint (c) {
+    if (c === undefined) {
+      super.print();
+    } else {
+      const str = c;
+      const line = str.repeat(this.width);
+      for (let i = 0; i < this.height; i++) {
+        console.log(line);
+      }
+    }
+  }
+}
+
+module.exports = Square;
