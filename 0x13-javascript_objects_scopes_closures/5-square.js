2074d6ddb4ec90d6768155127fa3f366eb2e2328
Nate Petersen <nate.petersen@me.com>
2019-09-23 17:29:56 +0000

    complete task 5 - add new class 'Square' that imports from class Rectangle.

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,1 +1,45 @@
 #!/usr/bin/node
+/* 5. Write class Square that inherits from Rectangle. */
+
+// Rectangle
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  // print representation of rectangle object.
+  print () {
+    const str = 'X';
+    const line = str.repeat(this.width);
+    for (let i = 0; i < this.height; i++) {
+      console.log(line);
+    }
+  }
+
+  // rotate width and height.
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  // double width and height of object.
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+
+// Square
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+    this.size = size;
+  }
+}
+
+module.exports = Rectangle;
+module.exports = Square;
