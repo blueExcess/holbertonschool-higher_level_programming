dec1be4e5d064fc621682576531d72f26ee3df0c
Nate Petersen <nate.petersen@me.com>
2019-09-23 15:55:32 +0000

    complete task 4 - methods to rotate and double size of objects.

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -2,14 +2,15 @@
 /* 4. add methods for rotating and doubling rectangle. */
 
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
 
-  print() {
+  // print representation of rectangle object.
+  print () {
     const str = 'X';
     const line = str.repeat(this.width);
     for (let i = 0; i < this.height; i++) {
@@ -17,7 +18,18 @@ class Rectangle {
     }
   }
 
+  // rotate width and height.
+  rotate () {
+    let temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+    }
 
+  // double width and height of object.
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+    }
 }
 
-module.exports = Rectangle
+module.exports = Rectangle;
