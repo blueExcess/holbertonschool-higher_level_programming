cf068f086cc6846278bb222bba673570c4f92977
Nate Petersen <nate.petersen@me.com>
2019-09-24 17:17:48 +0000

    setup and complete task 0 - read file and print with error printing.

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/* 0. open and print contents of a file and print error object. */
+
+const fs = require('fs');
+const source = process.argv[2];
+
+fs.readFile(source, 'utf8', (err, source) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(source);
+});
